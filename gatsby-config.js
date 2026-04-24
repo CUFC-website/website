234a04c3fea65e6f9f006276ac74252f461643db
CUFC-website <avian.sonar-8o@icloud.com>
2026-04-24 18:28:44 +0100

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Bright Studio`,
+    description: `A clean Gatsby starter website for a modern digital studio.`,
+    siteUrl: `https://www.example.com`,
+  },
+  plugins: [],
+}
